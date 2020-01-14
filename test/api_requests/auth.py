02727846c87dd029bc8188ccbdec3790b26b0004
ptkmartynov <59675662+ptkmartynov@users.noreply.github.com>
2020-01-14 09:14:11 +0700

    fix API test
    some fix API.
    DB test complete

--- test/api_requests/auth.py
+++ test/api_requests/auth.py
@@ -9,10 +9,11 @@ API_VER = ':7024/api/v1'
 
 API_PATH = HOST + API_VER
 
+#Логин. Получение токена
+
 def loginapi():
     url = 'http://grafin-uranus.rd.ptsecurity.ru:7024/api/v1/account/login'
    # url = API_PATH + '/account/login'
-#    headers = {'Content-Type': 'application/json'}
     data = {"login" : "polesurka",
  "password" : "P@ssw0rd"}
     r = requests.post(url, json=data)
@@ -23,29 +24,52 @@ def loginapi():
         print('Ответ сервера: ', r)
         raise Exception('Не удалось получить токен')
 
-    #token = r.content
-    #return token
-    token = json.loads(r)
-    print(token)
-    return token.get('token')
+    token = r.text
+    #print(token)
+    return token
 
+token = loginapi()
 
-#print(loginapi())
-
+#Получение ИНН
 
 def get_inn():
 
     url = 'http://grafin-uranus.rd.ptsecurity.ru:7024/api/v1/antifraud/feeds/hashPassport/download'
-    #r = requests.get(url, token=loginapi())
-    headers = {'Authorization': 'Bearer ' + loginapi()}
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + str(token)}
     r = requests.get(url, headers=headers)
-    print(r.content)
+    #print(headers)
+    #print(r.content)
 
-    if r.status_code != 200:
-        print('Ответ сервера: ', r.text)
-        raise Exception('Не удалось получить токен')
+    resp = r.text
+    return resp
 
-    return r.json()
+resp = get_inn()
+#resp1 = list(resp)
+#print(type(resp))
 
-print(get_inn())
+get_inn()
 
+#-----------------
+
+testapicheck = "40bdd9a947832a944894eb21e57f8a1b58496b4fff69f7208c52229925239be9"
+
+#Проверка, есть ли интересующий ИНН в GET запросе
+
+def check(x):
+
+    resp1 = resp.split('RUS')
+    #resp1 = resp.find(x)
+    #return resp1
+    #print(resp1)
+    resp2 = list(resp1)
+    print(type(resp2))
+    #return resp2
+    for i in x:
+        if testapicheck in i:
+            print("yes", testapicheck, i)
+            break
+    if testapicheck not in i:
+        print('no data')
+
+    return
+print(check(testapicheck))
